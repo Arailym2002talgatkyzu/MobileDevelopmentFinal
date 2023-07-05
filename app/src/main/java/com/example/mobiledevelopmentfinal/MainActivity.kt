1d48078d232962d0eedb0bb33fb096e181fa5651
AkimbetovaAyaulym <64066762+AkimbetovaAyaulym@users.noreply.github.com>
2023-07-05 19:09:33 +0200

    New

--- app/src/main/java/com/example/mobiledevelopmentfinal/MainActivity.kt
+++ app/src/main/java/com/example/mobiledevelopmentfinal/MainActivity.kt
@@ -5,12 +5,15 @@ import android.os.Bundle
 import android.widget.Toast
 import com.example.mobiledevelopmentfinal.data.utils.RetrofitInstance
 import com.example.mobiledevelopmentfinal.databinding.ActivityMainBinding
+import com.squareup.picasso.Picasso
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import retrofit2.HttpException
 import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -38,7 +41,39 @@ class MainActivity : AppCompatActivity() {
 
             if(response.isSuccessful && response.body()!=null){
                 withContext(Dispatchers.Main){
-                    binding.tvTemp.text
+                    val data =response.body()!!
+                    val iconId = data.weather[0].icon
+
+                    val imgurl ="https://openweathermap.org/img/wn/$iconId@4x.png"
+                    Picasso.get().load(imgurl).into(binding.imgWeather)
+                    binding.tvSunrise.text=
+                        SimpleDateFormat(
+                            "hh:mm a",
+                            Locale.ENGLISH
+                        ).format(data.sys.sunrise*1000)
+
+                    binding.tvSunset.text=
+                        SimpleDateFormat(
+                            "hh:mm a",
+                            Locale.ENGLISH
+                        ).format(data.sys.sunset*1000)
+                    binding.apply {
+                        tvStatus.text=data.weather[0].description
+                        tvWind.text="${data.wind.speed.toString()} KM/H"
+                        tvLocation.text = "${data.name}\n${data.sys.country}"
+                        tvTemp.text = "${data.main.temp.toInt()}°C"
+                        tvFeelsLike.text = "Feels like: ${data.main.feels_like.toInt()}°C"
+                        tvMinTemp.text = "Min temp: ${data.main.temp_min.toInt()}°C"
+                        tvMaxTemp.text = "Max temp: ${data.main.temp_max.toInt()}°C"
+                        tvHumidity.text = "${data.main.humidity} %"
+                        tvPressure.text = "${data.main.pressure} hPa"
+                        tvUpdateTime.text = "Last updated: ${
+                            SimpleDateFormat(
+                                "hh:mm a",
+                                Locale.ENGLISH
+                            ).format(data.dt*1000)
+                        }"
+                    }
                 }
             }
         }
